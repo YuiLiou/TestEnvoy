543f38b66f483998a74f7356cc5cd9d2229902cf
YuiLiou <ko8722375@gmail.com>
2025-08-12 19:05:59 +0800

    feat: hard code instance list

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from prometheus_client import Counter, generate_latest, Gauge, CONTENT_TYPE_LATEST
+import random
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint'])
+
+MY_GAUGE = Gauge('my_gauge', 'description')
+MY_GAUGE.set(123)
+
+@app.before_request
+def before_request_func():
+    REQUEST_COUNT.labels(method=request.method, endpoint=request.path).inc()
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, world!</p>"
+    MY_GAUGE.inc()
+    return "Hello, world!"
 
 @app.route("/health")
 def health_check():
-    return "<p>API is available!</p>"
+    return "API is available!"
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type':'text/plain; version=0.0.4; charset=utf-8'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 8299)
